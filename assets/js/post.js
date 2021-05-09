d78f9a2714f676a7f98d652b7f91d31830036466
Himeyama <Himeyama@users.noreply.github.com>
2021-05-09 11:34:30 +0000

    deploy: 43fd28c42dfd59b79cc2cb46851120bb16535efa

--- assets/js/post.js
+++
@@ -1,7 +0,0 @@
-function date_post(){
-    let e = document.getElementsByClassName("post-d")
-    for(f of e)
-        f.innerHTML = (new Era(f.innerHTML)).getWareki()
-}
-
-date_post()
--- monaco.md
+++
@@ -1,8 +0,0 @@
----
-layout: post
-date: 2021-05-09 7:21
----
-
-<div id="monaco-container"></div>
-<script src="/assets/js/monaco.js">
-</script>
